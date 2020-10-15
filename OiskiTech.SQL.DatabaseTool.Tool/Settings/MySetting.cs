e96045689291c5815ea888a5fd0154872847d887
Mike Mortensen <mike2137@elevcampus.dk>
2020-10-15 13:00:25 +0200

    Initial Upload + Added Documentation

---
+++ OiskiTech.SQL.DatabaseTool.Tool/Settings/MySetting.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace OiskiTech.SQL.DatabaseTool
+{
+    /// <summary>
+    /// Represents a setting with a name and a value
+    /// </summary>
+    public class MySetting
+    {
+        /// <summary>
+        /// This is treated as a unique key by the <see cref="MySettingsCollection"/>
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// This is the actual setting
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// This will check the <see cref="Name"/> for a <see langword="null"/> or empty value
+        /// <br/>
+        /// <strong>Do not use this to check for <see langword="null"/> on an <see cref="MySetting"/> instance</strong>
+        /// </summary>
+        /// <returns><see langword="true"/> if the <see cref="Name"/> was <see langword="null"/> or empty</returns>
+        public bool IsEmpty()
+        {
+            if ( string.IsNullOrEmpty(Name) )
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Initialize a new <see langword="instance"/> of type <see cref="MySetting"/> with its <see langword="default"/> <see langword="values"/>
+        /// <br/>
+        /// <strong>Note:</strong> A <see cref="MySetting"/> with <see langword="default"/> <see langword="value"/> is consideret empty
+        /// </summary>
+        public MySetting()
+        {
+            Name = null;
+        }
+
+        /// <summary>
+        /// This will initialize a new instance of the <see cref="MySetting"/> where the <see cref="Name"/> and the <see cref="Value"/> of the setting is set
+        /// </summary>
+        /// <param name="_name"></param>
+        /// <param name="_value"></param>
+        public MySetting(string _name, string _value)
+        {
+            if ( !string.IsNullOrEmpty(_name) )
+            {
+                Name = _name;
+                Value = _value;
+            }
+            else
+            {
+                throw new ArgumentNullException("_name", "Null is not a valid value");
+            }
+        }
+    }
+}
---
+++ OiskiTech.SQL.DatabaseTool.Tool/XML/XMLIO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace OiskiTech.SQL.DatabaseTool
+{
+    /// <summary>
+    /// Represents a simple I/O system for handling XML files
+    /// </summary>
+    internal static class XMLIO
+    {
+        /// <summary>
+        /// Serialize an object of type <typeparamref name="T"/>, where <typeparamref name="T"/> is any serializable <see cref="object"/>.
+        /// </summary>
+        /// <typeparam name="T">The type to serialize</typeparam>
+        /// <param name="_obj">The object to serialize</param>
+        /// <param name="_path">The folder structure path to the file, including the file name but without the file extension</param>
+        public static void SerializeXML<T>(T _obj, string _path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            Stream stream = new FileStream($"{_path}.xml", FileMode.Create);
+            XmlWriter writer = new XmlTextWriter(stream, Encoding.Unicode);
+            serializer.Serialize(writer, _obj);
+            writer.Close();
+        }
+
+        /// <summary>
+        /// Deserialize an object of type <typeparamref name="T"/>, where <typeparamref name="T"/> is any deserializable <see cref="object"/>.
+        /// </summary>
+        /// <param name="_path">The folder structure path to the file, including the file name but without the file extension</param>
+        /// <returns>A new <see langword="instance"/> of type <typeparamref name="T"/> where all <see langword="values"/> are set according to deserialized XML batch</returns>
+        public static T DeserializeXML<T>(string _path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            T result;
+
+            using ( TextReader reader = File.OpenText($"{_path}.xml") )
+            {
+                result = ( T ) serializer.Deserialize(reader);
+            }
+
+            return result;
+        }
+    }
+}
